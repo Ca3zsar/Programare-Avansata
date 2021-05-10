fdbab9380332a8004d168607fbc36fae5684d330
Cezar Todirisca <cezartodirisca@gmail.com>
2021-05-10 17:23:48 +0300

    finished compulsory

--- Laborator11/Compulsory/Server/src/main/java/com/server/Main.java
+++ Laborator11/Compulsory/Server/src/main/java/com/server/Main.java
@@ -3,6 +3,13 @@ package com.server;
 public class Main {
 
     public static void main(String[] args) {
+//        try {
+//            ScriptRunner scriptRunner = new ScriptRunner(JDBCConnection.getInstance().getConnection());
+//            Reader reader = new BufferedReader(new FileReader("C:\\Users\\cezar\\Desktop\\Sem2\\Programare-Avansata\\Laborator11\\script.sql"));
+//
+//            scriptRunner.runScript(reader);
+//        }
+
 	    ServerManager server = new ServerManager(8081);
 	    server.runServer();
     }
--- Laborator11/script.sql
+++ Laborator11/script.sql
@@ -5,7 +5,7 @@ DROP TABLE friendships;
 DROP SEQUENCE messageSequence;
 DROP SEQUENCE friendshipSequence;
 
-CREATE TABLE users (username VARCHAR2(50));
+CREATE TABLE users (id NUMBER, username VARCHAR2(50));
 CREATE TABLE messages (message_id NUMBER, sender VARCHAR2(100),receiver VARCHAR2(100), content VARCHAR2(100));
 CREATE TABLE friendships (friendship_id NUMBER, first_user VARCHAR2(50), second_user VARCHAR2(50));
 
